c7ee8a8aadc9ddb900b12e14008cbccd622d6d88
thedarkcoderrises <abhikrish88@gmail.com>
2020-01-12 14:37:18 +0530

    DataLoader not working :(

--- src/main/java/com/tdcr/graphql/config/GraphQLConfig.java
+++ src/main/java/com/tdcr/graphql/config/GraphQLConfig.java
@@ -1,21 +1,42 @@
 package com.tdcr.graphql.config;
 
+import com.tdcr.graphql.dao.repository.AddressRepository;
+import com.tdcr.graphql.dao.repository.BaseDao;
+import com.tdcr.graphql.dao.repository.PersonRepository;
+import com.tdcr.graphql.dao.repository.VehicleRepository;
+import com.tdcr.graphql.directives.AppDirectives;
 import com.tdcr.graphql.directives.UpperCaseDirective;
+import com.tdcr.graphql.mutation.PersonMutation;
+import com.tdcr.graphql.query.BaseQuery;
+import com.tdcr.graphql.query.PersonResolver;
+import com.tdcr.graphql.service.PersonService;
 import graphql.GraphQL;
+import graphql.Scalars;
 import graphql.execution.instrumentation.Instrumentation;
 import graphql.execution.instrumentation.dataloader.DataLoaderDispatcherInstrumentation;
+import graphql.introspection.Introspection;
+import graphql.schema.GraphQLArgument;
+import graphql.schema.GraphQLDirective;
+import graphql.schema.GraphQLNonNull;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.idl.*;
 import org.dataloader.DataLoaderRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
-
+import com.coxautodev.graphql.tools.SchemaParser;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.LinkedHashSet;
+
+import static graphql.introspection.Introspection.DirectiveLocation.*;
 
 
 @Configuration
@@ -26,6 +47,21 @@ import java.io.InputStreamReader;
 )
 public class GraphQLConfig {
 
+    @Autowired
+    private PersonService personService;
+
+    @Autowired
+    private PersonRepository personRepo;
+
+    @Autowired
+    private VehicleRepository vehicleRepo;
+
+    @Autowired
+    private AddressRepository addrsRepo;
+
+    @Autowired
+    private BaseDao baseDao;
+
     @Value("classpath:/graphql/vehicleql.graphqls")
     Resource resource;
 
@@ -35,35 +71,69 @@ public class GraphQLConfig {
     }
 
     @Bean
-    Instrumentation instrumentation(DataLoaderRegistry dataLoaderRegistry) {
-        return new DataLoaderDispatcherInstrumentation(dataLoaderRegistry);
+    Instrumentation instrumentation() {
+        DataLoaderDispatcherInstrumentation dldi = new DataLoaderDispatcherInstrumentation();
+        return dldi;
     }
 
+//    @Bean
+    /*public GraphQLSchema schema() throws IOException{
+        GraphQLSchema graphQLSchema;
+        InputStream inputStream = resource.getInputStream();
+        InputStreamReader streamReader = new InputStreamReader(inputStream);
+        TypeDefinitionRegistry typeDefinitionRegistry = new SchemaParser().parse(streamReader);
+
+        SchemaDirectiveWiring upperDirective = new UpperCaseDirective();
+        RuntimeWiring runtimeWiring = RuntimeWiring.newRuntimeWiring()
+//                .type("Query", typeWiring -> typeWiring.dataFetcher("person",baseQuery)))
+                .directive("upper", upperDirective)
+                .build();
+        graphQLSchema =new SchemaGenerator().makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+        return graphQLSchema;
+    }*/
+
+  @Bean
+  public BaseQuery baseQuery(PersonService personService,
+                             PersonRepository personRepo,
+                             VehicleRepository vehicleRepo,
+                             AddressRepository addrsRepo){
+      return new BaseQuery(personService,personRepo,vehicleRepo,addrsRepo);
+  }
+
+  @Bean
+  public PersonMutation personMutation(PersonService personService){
+      return new PersonMutation(personService);
+  }
+
+  @Bean
+  public PersonResolver personResolver(AddressRepository addressRepository,
+                                       VehicleRepository vehicleRepository,
+                                       BaseDao baseDao,
+                                       DataLoaderRegistry dataLoaderRegistry){
+     return new PersonResolver(addressRepository,vehicleRepository,baseDao,dataLoaderRegistry);
+  }
+
    @Bean
-   public GraphQL graphQL() throws IOException{
-       GraphQLSchema graphQLSchema;
-       InputStream inputStream = resource.getInputStream();
-       InputStreamReader streamReader = new InputStreamReader(inputStream);
-       TypeDefinitionRegistry typeDefinitionRegistry = new SchemaParser().parse(streamReader);
-       typeDefinitionRegistry.getDirectiveDefinitions();
+   public GraphQLSchema schema(BaseQuery baseQuery,
+                          PersonMutation personMutation,
+                          PersonResolver personResolver){
 
-       SchemaDirectiveWiring upperDirective = new UpperCaseDirective();
-       RuntimeWiring runtimeWiring = RuntimeWiring.newRuntimeWiring()
-               .directive("upper", upperDirective)
-               .build();
 
-       graphQLSchema =new SchemaGenerator().makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
-       GraphQL graphQL = GraphQL.newGraphQL(graphQLSchema).build();
-    return graphQL;
+       GraphQLSchema schema = SchemaParser.newParser().file("graphql/vehicleql.graphqls")
+               .resolvers(baseQuery)
+               .resolvers(personResolver)
+               .resolvers(personMutation)
+               .directive("upper", new UpperCaseDirective())
+               .build().makeExecutableSchema();
+
+       return  schema;
    }
 
-//   @Bean
-//   public GraphQL graphQL(GraphQLSchema schema){
-//       GraphQL graphQL = GraphQL.newGraphQL(schema).build();
-//       return  graphQL;
-//   }
-
-
+   @Bean
+    public GraphQL graphQL(GraphQLSchema schema){
+        return GraphQL.newGraphQL(schema)
+                .build();
+   }
 
 }
 
--- src/main/java/com/tdcr/graphql/directives/UpperCaseDirective.java
+++ src/main/java/com/tdcr/graphql/directives/UpperCaseDirective.java
@@ -1,8 +1,7 @@
 package com.tdcr.graphql.directives;
 
-import graphql.schema.DataFetcher;
-import graphql.schema.DataFetcherFactories;
-import graphql.schema.GraphQLFieldDefinition;
+import com.tdcr.graphql.dao.pojo.Person;
+import graphql.schema.*;
 import graphql.schema.idl.SchemaDirectiveWiring;
 import graphql.schema.idl.SchemaDirectiveWiringEnvironment;
 
@@ -10,24 +9,24 @@ public class UpperCaseDirective implements SchemaDirectiveWiring {
 
     @Override
     public GraphQLFieldDefinition onField(SchemaDirectiveWiringEnvironment<GraphQLFieldDefinition> environment) {
-        GraphQLFieldDefinition field = (GraphQLFieldDefinition)environment.getElement();
+       /* GraphQLFieldDefinition field = (GraphQLFieldDefinition)environment.getElement();
 
-        DataFetcher originalFetcher= null;// = field.getDataFetcher();
+        DataFetcher originalFetcher = field.getDataFetcher();
         DataFetcher dataFetcher = DataFetcherFactories.wrapDataFetcher(originalFetcher, ((dataFetchingEnvironment, value) -> {
             return value.toString().toUpperCase();
         }));
+         return field.transform( builder -> builder.dataFetcher(dataFetcher) );*/
+        GraphQLFieldDefinition field = (GraphQLFieldDefinition)environment.getElement();
+        GraphQLFieldsContainer parentType = environment.getFieldsContainer();
 
-         return field.transform( builder -> builder.dataFetcher(dataFetcher) );
-
-        /*GraphQLFieldDefinition field = GraphQLFieldDefinition.newFieldDefinition()
-                .name("name")
-                .type(Scalars.GraphQLString)
-//                .withDirective(GraphQLDirective.newDirective()
-//                        .name("upper"))
-                .dataFetcher(PropertyDataFetcher.fetching("sex"))
-                .build();
-
-        return field;*/
+        DataFetcher dataFetcher = new DataFetcher() {
+            @Override
+            public Object get(DataFetchingEnvironment dataFetchingEnvironment) throws Exception {
+                Person p = dataFetchingEnvironment.getSource();
+                return p.getName().toUpperCase();
+            }
+        };
+        return  field.transform( builder -> builder.dataFetcher(dataFetcher) );
     }
 }
 
@@ -35,4 +34,5 @@ public class UpperCaseDirective implements SchemaDirectiveWiring {
 //https://github.com/graphql-java/graphql-java/blob/master/src/test/groovy/readme/DirectivesExamples.java
 // https://www.graphql-java-kickstart.com/tools/directives/
 //https://github.com/graphql-java/graphql-java-extended-validation#the-supplied-directive-constraints
+//https://github.com/graphql-java-kickstart/samples/blob/master/directives/src/main/java/directives/UppercaseDirective.java
 //9527606697 Jayshree
